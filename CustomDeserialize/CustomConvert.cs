d994e5b4e7453dc8292291c85774d249cf6e4536
ahmadlie <eldar.ehmedli2003@mail.ru>
2023-12-26 08:00:40 -1200

    Add project files.

---
+++ CustomDeserialize/CustomConvert.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDeserialize
+{
+    public static class CustomConvert
+    {
+
+        public static TReturn Deserialize<TReturn>(string jsonString) where TReturn : class, new()
+        {
+            JObject jObject = JObject.Parse(jsonString);
+            TReturn Root = new TReturn();
+
+            foreach (var prop in jObject.Properties())
+            {
+                var targetProp = Root.GetType().GetProperty(prop.Name);
+                if (targetProp != null)
+                {
+                    targetProp.SetValue(Root, prop.Value.ToObject(targetProp.PropertyType));
+                }
+            }
+            return Root;
+        }
+    }
+}
---
+++ CustomDeserialize/Models/Dob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDeserialize.Models
+{
+    public class Dob
+    {
+        public DateTime date { get; set; }
+        public int age { get; set; }
+    }
+}
